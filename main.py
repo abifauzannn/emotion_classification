17e7fea66c71c845b2698460426ae39c51f880b3
abifauzan <mohammadabifauzan64@gmail.com>
2025-04-13 20:48:16 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Form
+from pydantic import BaseModel
+
+# Definisikan model untuk input request
+class TextRequest(BaseModel):
+    text: str
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+@app.get("/hello/{name}")
+def say_hello(name: str):
+    return {"message": f"Hello, {name}!"}
+
+@app.post("/casefold")
+def casefold_text(text: str = Form(...)):
+    casefolded_text = text.casefold()
+    return {"original_text": text, "casefolded_text": casefolded_text}
